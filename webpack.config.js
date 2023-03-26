09b1c83010c574d27560f524d3d3570726801c28
Matt Melito <matt_melito@outlook>
2023-03-26 09:40:38 -0700

    Add initial HTML, JS, JSON, and webpack files.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 const ESLintPlugin = require('eslint-webpack-plugin');
-const Dotenv = reuqire('ditebv-webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
