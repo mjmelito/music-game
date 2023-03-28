3e79633e9350d21f9a411bf47ab7b8997e7e3a13
Matt Melito <matt_melito@outlook>
2023-03-27 20:12:37 -0700

    Add iFrame player and CSS styling.

--- src/index.js
+++ src/index.js
@@ -1,102 +1,74 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import SpotifyService from "./spotify-service.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 
-// Define game logic functions
+const guessInput = document.getElementById("guess-input");
+
+// Initialize variables
+let accessToken = "";
+let playlistTracks = [];
+let currentTrackIndex = -1;
 let score = 0;
-let lives = 3;
-const correctAnswer = "The Name of the Correct Song";
 
-function incrementScore() {
-  score++;
-  const scoreDisplay = document.getElementById("score");
-  scoreDisplay.textContent = `Score: ${score}`;
-}
-
-function startRound() {
-  // play the preview of the first track from the playlist
-  SpotifyService.playTrack(access_token);
-  
-  // set the value of the correct-answer element to the correct song name
-  const correctAnswerElement = document.getElementById("correct-answer");
-  correctAnswerElement.textContent = correctAnswer;
-}
-
-function decrementLives() {
-  lives--;
-  const livesDisplay = document.getElementById("lives");
-  livesDisplay.textContent = `Lives: ${lives}`;
-}
-
-function endGame() {
-  // perform actions to end the game
-}
-
-function handleSubmission(event) {
-    event.preventDefault();
-    const guessInput = document.getElementById("guess-input");
-    const guess = guessInput.value.trim();
-    guessInput.value = "";
-  
-    if (!guess) {
-      return;
-    }
-  
-    const result = document.getElementById("result");
-    const correctAnswer = document.getElementById("correct-answer").textContent;
-    if (guess.toLowerCase() === correctAnswer.toLowerCase()) {
-      result.textContent = "Correct!";
-      result.classList.add("correct");
-      incrementScore();
-      startRound();
-    } else {
-      result.textContent = "Wrong!";
-      result.classList.add("wrong");
-      decrementLives();
-    }
-  
-    if (lives === 0) {
-      endGame();
-    }
-  }
-  
-  window.addEventListener("load", function () {
-    // Request token from Spotify API
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from(process.env.Client_ID + ':' + process.env.Client_Secret).toString('base64')
-      },
-      form: {
-        grant_type: 'client_credentials'
-      },
-      json: true
-    };
-  
-    fetch(authOptions.url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + Buffer.from(process.env.Client_ID + ':' + process.env.Client_Secret).toString('base64')
-      },
-      body: 'grant_type=client_credentials'
+// Load playlist when button is clicked
+function loadPlaylist() {
+  accessToken = document.getElementById("access-token-input").value;
+  fetch("https://api.spotify.com/v1/playlists/0sCbiYB4EZavMKH9gcbh8S/tracks", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then(function (response) {
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        throw new Error(errorMessage);
+      } else {
+        return response.json();
+      }
     })
-      .then(response => response.json())
-      .then(data => {
-        const access_token = data.access_token;
-        console.log('OAuth token:', access_token);
-  
-        // Use the access_token to make API calls
-        SpotifyService.playTrack(access_token);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  
-    document
-      .querySelector("form")
-      .addEventListener("submit", handleSubmission);
-  });
-  
-  
+    .then((data) => {
+      playlistTracks = data.items.map((item) => item.track);
+      nextSong();
+    })
+    .catch(function (error) {
+      return error;
+    });
+}
+
+// Submit guess when button is clicked
+function submitGuess() {
+  const currentTrack = playlistTracks[currentTrackIndex];
+  const guess = guessInput.value.trim().toLowerCase();
+  const correctAnswer = currentTrack.name.toLowerCase();
+  if (guess === correctAnswer) {
+    score++;
+    document.getElementById("score").textContent = score;
+    alert("Correct!");
+  } else {
+    alert("Incorrect. Try again!");
+  }
+}
+
+// Load and display the next song in the playlist
+function nextSong() {
+  currentTrackIndex++;
+  if (currentTrackIndex >= playlistTracks.length) {
+    currentTrackIndex = 0;
+  }
+  const currentTrack = playlistTracks[currentTrackIndex];
+  const trackUrl = `https://open.spotify.com/embed/track/${currentTrack.id}`;
+  document.getElementById("song-iframe").src = trackUrl;
+  guessInput.value = "";
+}
+
+window.addEventListener("load", function () {
+  document
+    .getElementById("load-playlist-button")
+    .addEventListener("click", loadPlaylist);
+  document
+    .getElementById("submit-guess-button")
+    .addEventListener("click", submitGuess);
+  document
+    .getElementById("next-song-button")
+    .addEventListener("click", nextSong);
+});
